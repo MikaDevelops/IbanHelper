1fb036f98ab9a0e71c16a745e0fd0a4975a959fa
mikavirtala <mika.virtala@gmail.com>
2025-09-13 09:02:37 +0300

    modulo 97 calculation, result to page

--- js/IbanHelper.js
+++ js/IbanHelper.js
@@ -101,29 +101,28 @@ class IbanHelper{
     }
 
     #checkMod(num){
+
         let startPart = "";
         let pointer = 0;
         let endPointer = 0;
+
         while (true){
-            let lenOfStart = startPart.length;
-            pointer = pointer + lenOfStart;
-            endPointer = pointer + 9 - lenOfStart;
-            
-            let pieceOfNum = num.slice(pointer, endPointer);
-            //let leftOvers = pie
-
+            endPointer = pointer + 9 - startPart.length;
+            let pieceOfNum = startPart + num.slice(pointer, endPointer);
+            startPart = String(Number(pieceOfNum)%97);
             pointer = endPointer;
-            
-
-            break;
+            if (pointer >= num.length) break; 
         }
+        
+        if (startPart === '1') return true;
+        else return false;
     }
 }
 
 /*
-0 1 2 3 4 5 6 7 8 9 10 11 12 13
-5 3 0 4 9 5 3 4 2 3  4  2  3  4
+0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21
+5 3 0 4 9 5 3 4 2 3  4  2  3  4  6  7  1  8  9  8  7  7
 ^                 ^
-                  ^  
+                  ^              ^
 
 */
